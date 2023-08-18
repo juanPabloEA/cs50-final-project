7cc27cbba65b4263b6e1bca2596ebd6ca75c53e5
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2023-08-18 17:03:04 -0400

    web - add - basic layout

--- web/myApp/src/app/app-routing.module.ts
+++ web/myApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) },
+  { path: '',   redirectTo: '/app', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
